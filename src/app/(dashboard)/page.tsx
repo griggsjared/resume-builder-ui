e29cfe2c6f55759e3f2c808ac33635aa4e7886f0
Jared Griggs <griggsjared@gmail.com>
2023-10-28 16:36:02 -0600

    replace title markup on dash with PageTitleBar

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,9 @@
+import { PageTitleBar } from "./_components/page-title-bar";
+
 export default function Page() {
   return (
     <>
-      <div className="flex items-center justify-between space-y-2">
-        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-      </div>
+      <PageTitleBar title="Dashboard" />
       <div>
         This is some sort of nice dashboard where the user can jump into the
         app.
